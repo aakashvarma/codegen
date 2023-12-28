cbe190fe719b9d7a1032e1d9c9d586267e6bad8d
aakashvarma <aakashvarma98@outlook.com>
2023-12-29 00:44:56 +0530

    add eos ad pad token

--- model_operators/quantize.py
+++ model_operators/quantize.py
@@ -90,6 +90,9 @@ class Quantizer:
             transformers.AutoTokenizer: The fine-tuning model tokenizer.
         """
         tokenizer = AutoTokenizer.from_pretrained(self.model_config.model_name)
+        if tokenizer.pad_token is None:
+            # tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+            tokenizer.pad_token = tokenizer.eos_token
         return tokenizer
 
     def model_setup(self):
