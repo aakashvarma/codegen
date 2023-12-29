7e6142338518e99429e25dbdc7d6689350deb1cd
aakashvarma <aakashvarma98@outlook.com>
2023-12-29 22:09:32 +0530

    print mismatched

--- utils/accuracy.py
+++ utils/accuracy.py
@@ -1,11 +1,20 @@
 import pickle
 import argparse
 
+
 def calculate_accuracy(sql_output_arr, real_output_arr):
-    correct_predictions = sum(1 for sql_output, real_output in zip(sql_output_arr, real_output_arr) if sql_output == real_output)
+    mismatched_indices = [i for i, (sql_output, real_output) in enumerate(zip(sql_output_arr, real_output_arr)) if
+                          sql_output != real_output]
+
+    print("Mismatched predictions:")
+    for index in mismatched_indices:
+        print(f"Index {index}: SQL Output: {sql_output_arr[index]}, Real Output: {real_output_arr[index]}")
+
+    correct_predictions = len(sql_output_arr) - len(mismatched_indices)
     total_predictions = len(sql_output_arr)
     accuracy = correct_predictions / total_predictions
     return accuracy
+
 
 def main():
     parser = argparse.ArgumentParser(description='Calculate accuracy from pickled data.')
@@ -19,6 +28,7 @@ def main():
     accuracy = calculate_accuracy(loaded_sql_output_arr, loaded_real_output_arr)
 
     print(f'Accuracy: {accuracy * 100:.2f}%')
+
 
 if __name__ == "__main__":
     main()
