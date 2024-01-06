a965a169aa84aa0b6479f35bfbe8e270b7ff85ab
aakashvarma <aakashvarma98@outlook.com>
2024-01-06 11:57:37 +0530

    change epochs and log steps

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -144,22 +144,8 @@ You must output the SQL query that answers the question.
         training_arguments = self.configure_training_arguments()
 
         def compute_metrics(eval_preds):
-            # references = pred.label_ids
-            # generated_texts = pred.predictions
-            # print("references:", references.size)
-            # print("generated_texts:", generated_texts.size)
-            # print()
-            #
-            # bleu_scores = []
-            # for reference, generated_text in zip(references, generated_texts):
-            #     reference_text = train_dataset[reference]['text']
-            #     bleu_score = sentence_bleu([reference_text], generated_text)
-            #     bleu_scores.append(bleu_score)
-
             preds, labels = eval_preds
             metric = evaluate.load("rouge")
-
-            import pdb; pdb.set_trace()
 
             if isinstance(preds, tuple):
                 preds = preds[0]
@@ -219,7 +205,8 @@ You must output the SQL query that answers the question.
             fp16=self.trainer_config.compute_type == "fp16",
             bf16=self.trainer_config.compute_type == "bf16",
             max_grad_norm=self.trainer_config.max_grad_norm,
-            max_steps=self.trainer_config.max_steps,
+            # max_steps=self.trainer_config.max_steps,
+            num_train_epochs=2,
             warmup_ratio=self.trainer_config.warmup_ratio,
             group_by_length=True,
             lr_scheduler_type=self.trainer_config.lr_scheduler_type,
